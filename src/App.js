87e5f3adcb1d0b76f4db5055dbb41a9301c64965
swarnjit <swarnjitchahal@gmail.com>
2020-04-11 12:27:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [data, setData] = useState({ response: [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://covid-193.p.rapidapi.com/statistics",
+        {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key":
+              "8529a60fe1msh86abdd1bb20286cp190477jsnc68714d9e22c",
+          },
+        }
+      );
+      setData(result.data);
+      console.log(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {data.response.map((item) => (
+        <li key={item.id}>
+          Country-{item.country}-- New-{item.cases.new}, Active-
+          {item.cases.active}
+        </li>
+      ))}
+    </ul>
   );
 }
-
 export default App;
