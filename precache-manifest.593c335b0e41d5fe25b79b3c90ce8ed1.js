cdfdb31065fe7d656929118e964197451a4f1a92
swarnjit <swarnjitchahal@gmail.com>
2020-04-11 12:46:43 -0400

    Updates

---
+++ precache-manifest.593c335b0e41d5fe25b79b3c90ce8ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6b9917a6486fe4f9d8f8c5c3106e9c",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "282cbb331ca18afd26c4",
+    "url": "/covid-19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc466e81c62fc5f61774",
+    "url": "/covid-19/static/js/2.7442bfc2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19/static/js/2.7442bfc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "282cbb331ca18afd26c4",
+    "url": "/covid-19/static/js/main.9fbfc923.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  }
+]);
