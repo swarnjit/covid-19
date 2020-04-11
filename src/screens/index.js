acfc12f616083dab5d7c27bcc856f546f206a968
swarnjit <swarnjitchahal@gmail.com>
2020-04-11 13:10:10 -0400

    Use Table from material-ui to make data easier to read

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Table,
+  TableContainer,
+  Paper,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 import axios from "axios";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
 function Home() {
+  const classes = useStyles();
   const [data, setData] = useState({ response: [] });
   useEffect(() => {
     const fetchData = async () => {
@@ -29,6 +49,36 @@ function Home() {
           <Typography>COVID-19 Tracker</Typography>
         </Toolbar>
       </AppBar>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Country</TableCell>
+              <TableCell align="right">Total Cases</TableCell>
+              <TableCell align="right">New Cases</TableCell>
+              <TableCell align="right">Active Cases</TableCell>
+              <TableCell align="right">Recovered</TableCell>
+              <TableCell align="right">Deaths</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.response.map((item) => (
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  {item.country}
+                </TableCell>
+                <TableCell align="right">{item.cases.total}</TableCell>
+                <TableCell align="right">{item.cases.new}</TableCell>
+                <TableCell align="right">{item.cases.active}</TableCell>
+                <TableCell align="right">{item.cases.recovered}</TableCell>
+                <TableCell align="right" style={{ background: "red" }}>
+                  {item.deaths.total}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <ul>
         {data.response.map((item) => (
           <li key={item.id}>
