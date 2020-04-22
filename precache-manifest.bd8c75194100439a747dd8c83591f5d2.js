c7f440f005382b00927b9ecdaeb6e76352bd7d0c
swarnjit <swarnjitchahal@gmail.com>
2020-04-22 11:56:25 -0400

    Updates

---
+++ precache-manifest.bd8c75194100439a747dd8c83591f5d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d55e9ea0a845e3fb5cfcf97bcc48ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dba97fb500868d1befb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dbbeb8774946e4f418bf",
+    "url": "/static/js/2.efd29f59.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.efd29f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dba97fb500868d1befb",
+    "url": "/static/js/main.ab9814a8.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  }
+]);
